6fa38e57a17d45e18465513150c58e679b01538d
renpet-hhh <renandcsc@gmail.com>
2020-04-18 16:43:17 -0300

    Add setRepeatMode

--- android/src/main/java/com/guichaguri/trackplayer/module/MusicModule.java
+++ android/src/main/java/com/guichaguri/trackplayer/module/MusicModule.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import com.facebook.react.bridge.*;
 import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.Player;
 import com.guichaguri.trackplayer.service.MusicBinder;
 import com.guichaguri.trackplayer.service.MusicService;
 import com.guichaguri.trackplayer.service.Utils;
@@ -147,6 +148,11 @@ public class MusicModule extends ReactContextBaseJavaModule implements ServiceCo
         constants.put("RATING_4_STARS", RatingCompat.RATING_4_STARS);
         constants.put("RATING_5_STARS", RatingCompat.RATING_5_STARS);
         constants.put("RATING_PERCENTAGE", RatingCompat.RATING_PERCENTAGE);
+
+        // Repeat modes
+        constants.put("REPEAT_MODE_OFF", Player.REPEAT_MODE_OFF);
+        constants.put("REPEAT_MODE_ONE", Player.REPEAT_MODE_ONE);
+        constants.put("REPEAT_MODE_ALL", Player.REPEAT_MODE_ALL);
 
         return constants;
     }
@@ -286,6 +292,14 @@ public class MusicModule extends ReactContextBaseJavaModule implements ServiceCo
     public void removeUpcomingTracks(final Promise callback) {
         waitForConnection(() -> {
             binder.getPlayback().removeUpcomingTracks();
+            callback.resolve(null);
+        });
+    }
+
+    @ReactMethod
+    public void setRepeatMode(int repeatMode, final Promise callback) {
+        waitForConnection(() -> {
+            binder.getPlayback().setRepeatMode(repeatMode);
             callback.resolve(null);
         });
     }
